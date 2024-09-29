462ce6f508d1dfe0f05b67c27a748620228d11d7
Rohit Nagbhidkar <rohit.nagbhidkar@aegonlife.com>
2024-09-29 19:13:24 +0530

    added initial push

---
+++ public/admin/precache-manifest.3b850e179f3609b4b83414061d2cef66.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85738597502c04668a7",
+    "url": "/css/AddViewKey.92af644c.css"
+  },
+  {
+    "revision": "0a16761556509032aeb5",
+    "url": "/css/AddViewKey~Billing~Categories~GSTSetting~Home-management~Order-details~Orders~Platforms~Products~Sel~bf1f35a9.238c8329.css"
+  },
+  {
+    "revision": "8acf33c159677ded0645",
+    "url": "/css/AddViewKey~Billing~Categories~Home-management~Order-details~Orders~Platforms~Products~SellProducts~S~9f966042.18bc4a4a.css"
+  },
+  {
+    "revision": "eb51a04ff4f9f7506e8a",
+    "url": "/css/AddViewKey~Billing~GSTSetting~Home-management~Order-details~Orders~Products~invoice~product.de6f792e.css"
+  },
+  {
+    "revision": "b16180fba04322d3437c",
+    "url": "/css/Billing.59f5ed52.css"
+  },
+  {
+    "revision": "84198c3a01f252d0408f",
+    "url": "/css/Categories.c2809a06.css"
+  },
+  {
+    "revision": "1d0bb9d8ae7700ab2114",
+    "url": "/css/GSTSetting.e786f09e.css"
+  },
+  {
+    "revision": "1162a4dfc4780a59ab81",
+    "url": "/css/GSTSetting~Order-details~Orders~Sellers~SuperAdmin.323ca788.css"
+  },
+  {
+    "revision": "f8d9ec509be556f5bb8d",
+    "url": "/css/Home-management.237c7945.css"
+  },
+  {
+    "revision": "86474c716ad579859927",
+    "url": "/css/Login.fc8b523c.css"
+  },
+  {
+    "revision": "c9b6a4c3335142a4576a",
+    "url": "/css/Order-details.3828395d.css"
+  },
+  {
+    "revision": "e4ed4f65e75fe4898564",
+    "url": "/css/Orders.c499bab2.css"
+  },
+  {
+    "revision": "522cc412a24e7c666575",
+    "url": "/css/Platforms.16c8d0af.css"
+  },
+  {
+    "revision": "db29dcd38b27cacfcd8b",
+    "url": "/css/Products.2062d989.css"
+  },
+  {
+    "revision": "60089cb82c0a96d4a64c",
+    "url": "/css/SellProducts.7951f02c.css"
+  },
+  {
+    "revision": "de0c27bb95511cc9700a",
+    "url": "/css/Sellers.a3ccd8dd.css"
+  },
+  {
+    "revision": "cf119f77c0678e29354c",
+    "url": "/css/Sub-Categories.84f129c4.css"
+  },
+  {
+    "revision": "781d221ca8af6b26c12b",
+    "url": "/css/SuperAdmin.a3ccd8dd.css"
+  },
+  {
+    "revision": "ae0fb3f52cb8ff77a133",
+    "url": "/css/app.480aafa8.css"
+  },
+  {
+    "revision": "fe41fe8a35c447da4a7c",
+    "url": "/css/chunk-vendors.4de36861.css"
+  },
+  {
+    "revision": "2994f2a725d50e9f62ac",
+    "url": "/css/invoice.96f7ab5c.css"
+  },
+  {
+    "revision": "437ef2ce6c8ea401c072",
+    "url": "/css/product.b6a33473.css"
+  },
+  {
+    "revision": "352bd1db2e97e52a09305d66f80937d9",
+    "url": "/img/carasoulV2-header.352bd1db.png"
+  },
+  {
+    "revision": "52b30a34a2fae15c16b0fd9b60e1ef5c",
+    "url": "/img/carasoulV2-placeholder.52b30a34.png"
+  },
+  {
+    "revision": "0b7856c9eed65fe901f54eb41ef37747",
+    "url": "/img/giftCard.0b7856c9.png"
+  },
+  {
+    "revision": "934e3b58c8637161dc2df44864db9e32",
+    "url": "/img/headerPlaceHolder.934e3b58.png"
+  },
+  {
+    "revision": "294de88d56b7cd0f3231a8e53ea80e3a",
+    "url": "/img/image-placeholder.294de88d.jpg"
+  },
+  {
+    "revision": "d92de74b79304f170528e3ba98886a05",
+    "url": "/img/logo.d92de74b.gif"
+  },
+  {
+    "revision": "2f0c170022c1c8835d23f7c1798ab7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85738597502c04668a7",
+    "url": "/js/AddViewKey.a4da6a58.js"
+  },
+  {
+    "revision": "0a16761556509032aeb5",
+    "url": "/js/AddViewKey~Billing~Categories~GSTSetting~Home-management~Order-details~Orders~Platforms~Products~Sel~bf1f35a9.4953e728.js"
+  },
+  {
+    "revision": "8acf33c159677ded0645",
+    "url": "/js/AddViewKey~Billing~Categories~Home-management~Order-details~Orders~Platforms~Products~SellProducts~S~9f966042.27b1dbd1.js"
+  },
+  {
+    "revision": "eb51a04ff4f9f7506e8a",
+    "url": "/js/AddViewKey~Billing~GSTSetting~Home-management~Order-details~Orders~Products~invoice~product.e3dce682.js"
+  },
+  {
+    "revision": "96fbd537993aca0dfc7b",
+    "url": "/js/AddViewKey~product.76439695.js"
+  },
+  {
+    "revision": "b16180fba04322d3437c",
+    "url": "/js/Billing.5ce768da.js"
+  },
+  {
+    "revision": "36a79bfa196a3b9d92fa",
+    "url": "/js/Billing~Categories~GSTSetting~Home-management~Platforms~Products~SellProducts~Sellers~Sub-Categories~a166f57b.dac6b32b.js"
+  },
+  {
+    "revision": "84198c3a01f252d0408f",
+    "url": "/js/Categories.f4b45864.js"
+  },
+  {
+    "revision": "1d0bb9d8ae7700ab2114",
+    "url": "/js/GSTSetting.5b105b0c.js"
+  },
+  {
+    "revision": "1162a4dfc4780a59ab81",
+    "url": "/js/GSTSetting~Order-details~Orders~Sellers~SuperAdmin.c7517819.js"
+  },
+  {
+    "revision": "f8d9ec509be556f5bb8d",
+    "url": "/js/Home-management.5ae8af0c.js"
+  },
+  {
+    "revision": "86474c716ad579859927",
+    "url": "/js/Login.84ecb368.js"
+  },
+  {
+    "revision": "c9b6a4c3335142a4576a",
+    "url": "/js/Order-details.5577e57f.js"
+  },
+  {
+    "revision": "e4ed4f65e75fe4898564",
+    "url": "/js/Orders.5aaf5640.js"
+  },
+  {
+    "revision": "522cc412a24e7c666575",
+    "url": "/js/Platforms.905d7038.js"
+  },
+  {
+    "revision": "db29dcd38b27cacfcd8b",
+    "url": "/js/Products.bcacc35b.js"
+  },
+  {
+    "revision": "60089cb82c0a96d4a64c",
+    "url": "/js/SellProducts.004e7992.js"
+  },
+  {
+    "revision": "de0c27bb95511cc9700a",
+    "url": "/js/Sellers.4c242ffa.js"
+  },
+  {
+    "revision": "cf119f77c0678e29354c",
+    "url": "/js/Sub-Categories.458edddb.js"
+  },
+  {
+    "revision": "781d221ca8af6b26c12b",
+    "url": "/js/SuperAdmin.7f634ccc.js"
+  },
+  {
+    "revision": "448ac46f99beaae065d7",
+    "url": "/js/about.42d542dc.js"
+  },
+  {
+    "revision": "ae0fb3f52cb8ff77a133",
+    "url": "/js/app.d1fe6b4a.js"
+  },
+  {
+    "revision": "fe41fe8a35c447da4a7c",
+    "url": "/js/chunk-vendors.c4d11d37.js"
+  },
+  {
+    "revision": "2994f2a725d50e9f62ac",
+    "url": "/js/invoice.4bb4f1f3.js"
+  },
+  {
+    "revision": "437ef2ce6c8ea401c072",
+    "url": "/js/product.b44ccda2.js"
+  },
+  {
+    "revision": "a222bd473985c22f6a28d52792c5365b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ public/main/precache-manifest.20a4ace22153f263636a2afd04071eeb.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfc5173fd5f66472cbd",
+    "url": "/css/app.a707d174.css"
+  },
+  {
+    "revision": "280012348f8ff9ec1484",
+    "url": "/css/checkout.f6b8c4f8.css"
+  },
+  {
+    "revision": "7dee9afbbfec8aaa5e31",
+    "url": "/css/checkout~dashboard~personal details.2eab7850.css"
+  },
+  {
+    "revision": "a5560fbb7e30c09d8d3b",
+    "url": "/css/checkout~order-detail.50ad8c50.css"
+  },
+  {
+    "revision": "d422f31fdb93e3ee2863",
+    "url": "/css/chunk-vendors.82531984.css"
+  },
+  {
+    "revision": "84b2a8eb9e51c69ccbdc",
+    "url": "/css/dashboard.d67a747c.css"
+  },
+  {
+    "revision": "0b74ca3e63b61e30ea2a",
+    "url": "/css/dashboard~home~login~register.845cf496.css"
+  },
+  {
+    "revision": "46a46fd9285c42996392",
+    "url": "/css/dashboard~order-detail~orders.daca5a6e.css"
+  },
+  {
+    "revision": "881c9a1837bd289cee94",
+    "url": "/css/home.955387b9.css"
+  },
+  {
+    "revision": "d2f49ef623c5c5a07727",
+    "url": "/css/login.3a516679.css"
+  },
+  {
+    "revision": "3e0b41ebfb82589a679a",
+    "url": "/css/order-detail.15e13956.css"
+  },
+  {
+    "revision": "a3c0941ea38347e27597",
+    "url": "/css/orders.330c6e05.css"
+  },
+  {
+    "revision": "c5a761dad75b46cd0659",
+    "url": "/css/produt-page.f0c270c2.css"
+  },
+  {
+    "revision": "acb52ffb8aaff911797b",
+    "url": "/css/register.d6e2ad8b.css"
+  },
+  {
+    "revision": "b9f4bb883fe4270027e3",
+    "url": "/css/sell.4e9e1ab2.css"
+  },
+  {
+    "revision": "6b8aeecd542fde288a0ac0f7a5bfb560",
+    "url": "/fonts/metro-400-i.6b8aeecd.ttf"
+  },
+  {
+    "revision": "3440b60515f3e9b38279956ce0bd3e52",
+    "url": "/fonts/metro-400.3440b605.ttf"
+  },
+  {
+    "revision": "17d6c18e6a7290a23aafc9828dfa416c",
+    "url": "/fonts/metro-500.17d6c18e.ttf"
+  },
+  {
+    "revision": "a399fac009c02979b3502980a85fb32a",
+    "url": "/fonts/metro-700.a399fac0.ttf"
+  },
+  {
+    "revision": "c35bff3a7e786e37066ec2aad75e71a6",
+    "url": "/fonts/metro-900.c35bff3a.ttf"
+  },
+  {
+    "revision": "aeb6bb090a0ec47f8da87f20520d827c",
+    "url": "/img/Nintendo.aeb6bb09.svg"
+  },
+  {
+    "revision": "8b7f1a2766d1f64b2fca9b59f37c0738",
+    "url": "/img/PC.8b7f1a27.svg"
+  },
+  {
+    "revision": "ed58cf616e465cfc3b02d03eb9b3032e",
+    "url": "/img/Playstation.ed58cf61.svg"
+  },
+  {
+    "revision": "69a45515c880dbf0ba247433b85a2f77",
+    "url": "/img/SellBannerDesk.69a45515.png"
+  },
+  {
+    "revision": "e2fa42b6fc61367c08a537981b03c2f7",
+    "url": "/img/Xbox.e2fa42b6.svg"
+  },
+  {
+    "revision": "da5d7c7ba9de7fe2184476a67a8cecec",
+    "url": "/img/action.da5d7c7b.svg"
+  },
+  {
+    "revision": "86dc6c3cf91637a10620b5f31a83e074",
+    "url": "/img/adventure.86dc6c3c.svg"
+  },
+  {
+    "revision": "89c9dea7b0f6341a714177d799333648",
+    "url": "/img/arcade.89c9dea7.svg"
+  },
+  {
+    "revision": "518498180f8c23471c145b79d0eb019b",
+    "url": "/img/ban-xxl.51849818.png"
+  },
+  {
+    "revision": "f32cba2ca1ce9e91ba819063ebed307f",
+    "url": "/img/content-placeholder.f32cba2c.png"
+  },
+  {
+    "revision": "36948071c9e8962a5f49c71c3aee2588",
+    "url": "/img/educational.36948071.svg"
+  },
+  {
+    "revision": "116b2b0eaf71b1aefdd1b3354ab6a21c",
+    "url": "/img/exclaim.116b2b0e.png"
+  },
+  {
+    "revision": "8f6c57e988f12b25ddd72173eca6dd37",
+    "url": "/img/fbLogo.8f6c57e9.svg"
+  },
+  {
+    "revision": "9abb6a14ef9bedce443437516e85817d",
+    "url": "/img/fighting.9abb6a14.svg"
+  },
+  {
+    "revision": "1d1dbcd013c352b50c9614d668db37c4",
+    "url": "/img/fps.1d1dbcd0.svg"
+  },
+  {
+    "revision": "b7727941c0e8a117b6cfd8f06a1cb7ed",
+    "url": "/img/googleLogo.b7727941.svg"
+  },
+  {
+    "revision": "d2d40b58d7b34c1451bbd89ca1f69861",
+    "url": "/img/hack-slash.d2d40b58.svg"
+  },
+  {
+    "revision": "670ce8628f0da011a0c105c2daa9cffc",
+    "url": "/img/indie.670ce862.svg"
+  },
+  {
+    "revision": "b6e591ff06d0abd9f3046b7199cb321b",
+    "url": "/img/info.b6e591ff.svg"
+  },
+  {
+    "revision": "a4b9a353f8fd6e734ea766e29f9bf163",
+    "url": "/img/logo.a4b9a353.png"
+  },
+  {
+    "revision": "c5e831c1632144549d51055c97fa5bb6",
+    "url": "/img/mmo.c5e831c1.svg"
+  },
+  {
+    "revision": "9d838ec706e6a98068f47307a75f3da0",
+    "url": "/img/music.9d838ec7.svg"
+  },
+  {
+    "revision": "72c0d01bb938fd2577d4215a05c0ef30",
+    "url": "/img/platform.72c0d01b.svg"
+  },
+  {
+    "revision": "940c749b5a27eb843d0f3f06d988fdad",
+    "url": "/img/point-click.940c749b.svg"
+  },
+  {
+    "revision": "5cce1f300b5948c77c8a0b8a478d4e03",
+    "url": "/img/product-placeholder.5cce1f30.png"
+  },
+  {
+    "revision": "219ca3b76fe712bcb6340e607f22477e",
+    "url": "/img/puzzle.219ca3b7.svg"
+  },
+  {
+    "revision": "faa55152bbde94729c1384123b908558",
+    "url": "/img/racing.faa55152.svg"
+  },
+  {
+    "revision": "3374f1edd60bef088504b404beb5a7cb",
+    "url": "/img/rpg.3374f1ed.svg"
+  },
+  {
+    "revision": "828504e747b3c5f15df41be8ee84013a",
+    "url": "/img/simulation.828504e7.svg"
+  },
+  {
+    "revision": "e9cfceb3baadf519a801a15b9c77b06e",
+    "url": "/img/sport.e9cfceb3.svg"
+  },
+  {
+    "revision": "445efb1a031e2defc4869d9455cc5617",
+    "url": "/img/strategy.445efb1a.svg"
+  },
+  {
+    "revision": "b04a05669e3dc8a870f82db143b0414c",
+    "url": "/img/userGuide.b04a0566.svg"
+  },
+  {
+    "revision": "5eb6315965bc99ed72941f96f2160f2b",
+    "url": "/img/verified-icon.5eb63159.svg"
+  },
+  {
+    "revision": "26c10081a305e6c538cb5f1fab2b3c44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bfc5173fd5f66472cbd",
+    "url": "/js/app.d40c6d86.js"
+  },
+  {
+    "revision": "280012348f8ff9ec1484",
+    "url": "/js/checkout.2af39b28.js"
+  },
+  {
+    "revision": "09e6517670cbabc40519",
+    "url": "/js/checkout~dashboard~order-detail~orders.1394f88e.js"
+  },
+  {
+    "revision": "7dee9afbbfec8aaa5e31",
+    "url": "/js/checkout~dashboard~personal details.a60f69a4.js"
+  },
+  {
+    "revision": "a5560fbb7e30c09d8d3b",
+    "url": "/js/checkout~order-detail.ea5e513c.js"
+  },
+  {
+    "revision": "d422f31fdb93e3ee2863",
+    "url": "/js/chunk-vendors.fc0bca37.js"
+  },
+  {
+    "revision": "84b2a8eb9e51c69ccbdc",
+    "url": "/js/dashboard.376e9691.js"
+  },
+  {
+    "revision": "0b74ca3e63b61e30ea2a",
+    "url": "/js/dashboard~home~login~register.54c4fe64.js"
+  },
+  {
+    "revision": "46a46fd9285c42996392",
+    "url": "/js/dashboard~order-detail~orders.7646e784.js"
+  },
+  {
+    "revision": "881c9a1837bd289cee94",
+    "url": "/js/home.4187f574.js"
+  },
+  {
+    "revision": "d2f49ef623c5c5a07727",
+    "url": "/js/login.7db675af.js"
+  },
+  {
+    "revision": "3e0b41ebfb82589a679a",
+    "url": "/js/order-detail.e7055654.js"
+  },
+  {
+    "revision": "a3c0941ea38347e27597",
+    "url": "/js/orders.807344b7.js"
+  },
+  {
+    "revision": "4570f321857b4c1bf5c7",
+    "url": "/js/personal details.a2cdced1.js"
+  },
+  {
+    "revision": "c38fdb10db826b0ebd31",
+    "url": "/js/privacy-policy.7dd47af1.js"
+  },
+  {
+    "revision": "c5a761dad75b46cd0659",
+    "url": "/js/produt-page.8b414efb.js"
+  },
+  {
+    "revision": "acb52ffb8aaff911797b",
+    "url": "/js/register.dd486132.js"
+  },
+  {
+    "revision": "b9f4bb883fe4270027e3",
+    "url": "/js/sell.52a985d3.js"
+  },
+  {
+    "revision": "7156ec683d8c915bbc443c833d628462",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
